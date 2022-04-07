c6d11a4e0c7603fb5902745c87cc37e8b96e138d
Koki Takeyama <koki.takeyama@gmail.com>
2022-04-07 23:14:15 +0900

    add one-liner functionality

--- js.cmd/shell.js.cmd
+++ js.cmd/shell.js.cmd
@@ -1,6 +1,11 @@
 @if (0)==(0) REM: %~nx0 & @cscript.exe //e:jscript //nologo "%~f0" %* & GOTO :EOF
 @end
 
+if (WScript.Arguments.Length == 1) {
+    exec(WScript.Arguments(0).replace(/`/g,"\""));
+    WScript.Quit();
+}
+
 while (true) {
     WScript.StdOut.Write("js>");
     var line = WScript.StdIn.ReadLine();
