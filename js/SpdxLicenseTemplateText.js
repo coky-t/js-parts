1bceb418647a11c2907e9bcf6711a09bb9288632
Koki Takeyama <koki.takeyama@gmail.com>
2023-05-04 22:33:31 +0900

    spdx template to text

--- js/SpdxLicenseTemplateText.js
+++ js/SpdxLicenseTemplateText.js
@@ -53,8 +53,9 @@ function GetMatchingLines(TemplateText) {
 function GetMatchingText(TemplateText) {
     if (TemplateText == "") { return ""; }
     
+    // B.15.3 Legacy Text Template format
     var Pattern = "(?:" + 
-        "<<var;name=\"([^\"]+)\";original=\"([^\"]+)\";match=\"([^\"]+)\">>" + "|" +
+        "<<var;name=\"([^\"]+)\";original=\"(.+)\";match=\"(.+)\">>" + "|" +
         "<<beginOptional>>([^<]+)<<endOptional>>" + ")";
     
     if (RegExp_Test(TemplateText, Pattern, true, false)) {
@@ -88,7 +89,7 @@ function GetMatchingText(TemplateText) {
             // B.3.4 Guideline: replaceable text
             // B.8 Bullets and numbering
             // B.11 Copyright notice
-            // <<var;name="([^"]+)";original="([^"]+)";match="([^"]+)">>
+            // <<var;name="([^"]+)";original="(.+)";match="(.+)">>
             
             //var VarName;
             //var VarOriginal;
@@ -103,7 +104,7 @@ function GetMatchingText(TemplateText) {
             // B.3.5 Guideline: omittable text
             // B.12 License name or title
             // B.13 Extraneous text at the end of a license
-            // "<<beginOptional>>([^<]+)<<endOptional>>"
+            // "<<beginOptional>>(.+)<<endOptional>>"
             
             var OptText;
             OptText = SubMatch4;
@@ -138,8 +139,184 @@ function GetPlainText(TemplateText) {
     
     PlainText = PlainText.replace(/<<beginOptional>>/g, "");
     PlainText = PlainText.replace(/<<endOptional>>/g, "");
-    PlainText = RegExp_Replace(PlainText, "", "<<var;name=\"([^\"]+)\";original=\"", false, true, false);
-    PlainText = RegExp_Replace(PlainText, "", "\";match=\"([^\"]+)\">>", false, true, false);
+    PlainText =
+        RegExp_Replace(
+            PlainText,
+            "",
+            "<<var;name=\"([^\"]+)\";original=\"",
+            false, true, false);
+    PlainText = 
+        RegExp_Replace(
+            PlainText,
+            "",
+            "\";match=\"([^\"]+)\">>",
+            false, true, false);
     
     return PlainText;
+}
+
+function GetPlainTextEx(TemplateText) {
+    if (TemplateText == "") { return ""; }
+    
+    // B.15.3 Legacy Text Template format
+    //var Pattern = "(?:" + 
+    //    "<<var;name=\"([^\"]+)\";original=\"(.+)\";match=\"(.+)\">>" + "|" +
+    //    "<<beginOptional>>([^<]+)<<endOptional>>" + ")";
+    var Pattern = "(?:<<var;name=\"([^\"]+)\";original=\"|<<beginOptional>>)";
+    
+    if (! RegExp_Test(TemplateText, Pattern, true, false)) {
+        return TemplateText;
+    }
+    
+    var Matches;
+    Matches = RegExp_Execute(TemplateText, Pattern, true, false, false);
+    
+    var Match;
+    Match = Matches[0];
+    
+    var Match_FirstIndex;
+    Match_FirstIndex = TemplateText.indexOf(Match);
+    
+    var SubMatch1;
+    //var SubMatch2;
+    //var SubMatch3;
+    //var SubMatch4;
+    SubMatch1 = Matches[1];
+    //SubMatch2 = Matches[2];
+    //SubMatch3 = Matches[3];
+    //SubMatch4 = Matches[4];
+    
+    var PreviousText = "";
+    if (Match_FirstIndex > 0) {
+        PreviousText = TemplateText.substr(0, Match_FirstIndex);
+    }
+    
+    var MiddleText = "";
+    var MiddleTextTemp = "";
+    var PostText = "";
+    var PostTextTemp = "";
+    
+    if (SubMatch1 != "") {
+        // B.3.4 Guideline: replaceable text
+        // B.8 Bullets and numbering
+        // B.11 Copyright notice
+        // <<var;name="([^"]+)";original="(.+)";match="(.+)">>
+        
+        //var VarName;
+        //var VarOriginal;
+        //var VarMatch;
+        //VarName = SubMatch1;
+        //VarOriginal = SubMatch2;
+        //VarMatch = SubMatch3;
+        
+        //MiddleText = VarOriginal;
+        
+        //if (Match_FirstIndex + Match.length < TemplateText.length) {
+        //    PostTextTemp = 
+        //        TemplateText.substr(Match_FirstIndex + Match.length);
+        //    PostText = GetPlainTextEx(PostTextTemp);
+        //}
+        
+        var VarMatchPos;
+        var VarEndPos = -1;
+        
+        VarMatchPos =
+            TemplateText.indexOf(
+                "\";match=\"", 
+                Match_FirstIndex + Match.length);
+        
+        if (VarMatchPos >= 0) {
+            VarEndPos =
+                TemplateText.indexOf(
+                    "\">>",
+                    VarMatchPos + "\";match=\"".length);
+        }
+        
+        if (VarEndPos >= 0) {
+            MiddleTextTemp =
+                TemplateText.substr(
+                    Match_FirstIndex + Match.length,
+                    VarMatchPos - (Match_FirstIndex + Match.length));
+            MiddleText = MiddleTextTemp;
+            
+            if (VarEndPos + "\">>".length < TemplateText.length) {
+                PostTextTemp = TemplateText.substr(VarEndPos + "\">>".length);
+                PostText = GetPlainTextEx(PostTextTemp);
+            }
+            
+        }
+        
+    } else {
+        // B.3.5 Guideline: omittable text
+        // B.12 License name or title
+        // B.13 Extraneous text at the end of a license
+        // "<<beginOptional>>(.+)<<endOptional>>"
+        
+        //MiddleTextTemp = SubMatch4;
+        //MiddleText = GetPlainTextEx(MiddleTextTemp);
+        
+        //if (Match_FirstIndex + Match.length < TemplateText.length) {
+        //    PostTextTemp = 
+        //        TemplateText.substr(Match_FirstIndex + Match.length);
+        //    PostText = GetPlainTextEx(PostTextTemp);
+        //}
+        
+        var EndOptionalPos;
+        //EndOptionalPos =
+        //    TemplateText.indexOf(
+        //        "<<endOptional>>",
+        //        Match_FirstIndex + Match.length);
+        EndOptionalPos =
+            GetEndOptionalPos(
+                Match_FirstIndex + Match.length,
+                TemplateText)
+        if (EndOptionalPos >= 0) {
+            MiddleTextTemp =
+                TemplateText.substr(
+                    Match_FirstIndex + Match.length,
+                    EndOptionalPos - (Match_FirstIndex + Match.length));
+            MiddleText = GetPlainTextEx(MiddleTextTemp);
+            
+            if (EndOptionalPos + "<<endOptional>>".length <
+                TemplateText.length) {
+                PostTextTemp =
+                    TemplateText.substr(
+                        EndOptionalPos + "<<endOptional>>".length);
+                PostText = GetPlainTextEx(PostTextTemp);
+            }
+            
+        }
+        
+    }
+    
+    return PreviousText + MiddleText + PostText;
+}
+
+function GetEndOptionalPos(StartPos, TemplateText) {
+    var BeginOptionalPos;
+    BeginOptionalPos = TemplateText.indexOf("<<beginOptional>>", StartPos);
+    
+    var EndOptionalPos;
+    EndOptionalPos = TemplateText.indexOf("<<endOptional>>", StartPos);
+    
+    if (BeginOptionalPos < 0) {
+        return EndOptionalPos;
+    }
+    
+    if (BeginOptionalPos > EndOptionalPos) {
+        return EndOptionalPos;
+    }
+    
+    var EndOptionalPosTemp;
+    EndOptionalPosTemp =
+        GetEndOptionalPos(
+            BeginOptionalPos + "<<beginOptional>>".length,
+            TemplateText);
+    
+    EndOptionalPos =
+        GetEndOptionalPos(
+            EndOptionalPosTemp + "<<endOptional>>".length,
+            TemplateText);
+    
+    return EndOptionalPos;
 }
--- js_/Test_SpdxTextFile.js
+++ js_/Test_SpdxTextFile.js
@@ -84,6 +84,18 @@ function Test_SaveSpdxTemplateToTextFiles() {
         OutputDirPath, SpdxTextDirPath);
 }
 
+function Test_SaveSpdxTemplateToTextFilesEx() {
+    var OutputDirPath;
+    OutputDirPath = "C:\\work\\data\\spdx-license-template-to-text-ex";
+    
+    // https://github.com/spdx/license-list-data/tree/vX.XX/template
+    var SpdxTextDirPath
+    SpdxTextDirPath = "C:\\work\\data\\spdx-license-template";
+    
+    Test_SaveSpdxTemplateToTextFilesEx_Core(
+        OutputDirPath, SpdxTextDirPath);
+}
+
 //
 // --- Test Core ---
 //
@@ -110,7 +122,7 @@ function Test_SaveSpdxTextFile_Core(
             "\">" + ReplaceChars(FileText) + "</pre>\r\n";
     }
     
-    MADODBStream_WriteTextFileUTF8(OutputFilePath, OutputText, false);
+    MADODBStream_WriteTextFileUTF8(OutputFilePath, OutputText, true);
     Debug_Print("... Done.");
 }
 
@@ -136,7 +148,7 @@ function Test_SaveSpdxTemplateFile_Core(
             "\">" + ReplaceChars(FileText) + "</pre>\r\n"
     }
     
-    MADODBStream_WriteTextFileUTF8(OutputFilePath, OutputText, false);
+    MADODBStream_WriteTextFileUTF8(OutputFilePath, OutputText, true);
     Debug_Print("... Done.");
 }
 
@@ -174,7 +186,7 @@ function Test_SaveSpdxTextLinesFile_Core(
         }
     }
     
-    MADODBStream_WriteTextFileUTF8(OutputFilePath, OutputText, false);
+    MADODBStream_WriteTextFileUTF8(OutputFilePath, OutputText, true);
     Debug_Print("... Done.");
 }
 
@@ -213,7 +225,7 @@ function Test_SaveSpdxTemplateLinesFile_Core(
         }
     }
     
-    MADODBStream_WriteTextFileUTF8(OutputFilePath, OutputText, false);
+    MADODBStream_WriteTextFileUTF8(OutputFilePath, OutputText, true);
     Debug_Print("... Done.");
 }
 
@@ -268,5 +280,46 @@ function Test_SaveSpdxTemplateToTextFile_Core(
     var OutputText;
     OutputText = GetPlainText(InputText);
     
-    MADODBStream_WriteTextFileUTF8(OutputFilePath, OutputText, false);
+    MADODBStream_WriteTextFileUTF8(OutputFilePath, OutputText, true);
+}
+
+function Test_SaveSpdxTemplateToTextFilesEx_Core(
+    OutputDirPath, DirPath) {
+    
+    var Folder;
+    Folder = GetFileSystemObject().GetFolder(DirPath);
+    
+    var Files = new Enumerator(Folder.Files);
+    for (; !Files.atEnd(); Files.moveNext()) {
+        var File = Files.item();
+        
+        Debug_Print(File.Name);
+        
+        var InputFilePath;
+        InputFilePath = File.Path;
+        
+        var OutputFileName;
+        OutputFileName = File.Name.substring(0, 
+            File.Name.length - ".template.txt".length) + ".txt";
+        
+        var OutputFilePath;
+        OutputFilePath =
+            GetFileSystemObject().BuildPath(OutputDirPath, OutputFileName);
+        
+        Test_SaveSpdxTemplateToTextFileEx_Core(OutputFilePath, InputFilePath);
+    }
+    
+    Debug_Print("... Done.");
+}
+
+function Test_SaveSpdxTemplateToTextFileEx_Core(
+    OutputFilePath, InputFilePath) {
+    
+    var InputText;
+    InputText = MADODBStream_ReadTextFileUTF8(InputFilePath);
+    
+    var OutputText;
+    OutputText = GetPlainTextEx(InputText);
+    
+    MADODBStream_WriteTextFileUTF8(OutputFilePath, OutputText, true);
 }
